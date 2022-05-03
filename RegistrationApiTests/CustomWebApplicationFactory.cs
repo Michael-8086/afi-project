99509227b47b2af66d3b878cb2443e980ad01f14
Michael-8086 <58359899+Michael-8086@users.noreply.github.com>
2022-05-03 11:20:07 +0100

    Update CustomWebApplicationFactory.cs

--- RegistrationApiTests/CustomWebApplicationFactory.cs
+++ RegistrationApiTests/CustomWebApplicationFactory.cs
@@ -17,28 +17,6 @@ namespace RegistrationApiTests
             builder.ConfigureServices(services =>
             {
                 services.AddScoped<IRegistrationApiRepository, MockRegistrationApiRepository>();
-                // var descriptor = services.SingleOrDefault(
-                //     d => d.ServiceType ==
-                //         typeof(DbContextOptions<RegistrationApiContext>));
-
-                // services.Remove(descriptor);
-
-                // services.AddDbContext<RegistrationApiContext>(options =>
-                // {
-                //     options.UseInMemoryDatabase("InMemoryDbForTesting");
-                // });
-
-                // var sp = services.BuildServiceProvider();
-
-                // using (var scope = sp.CreateScope())
-                // {
-                //     var scopedServices = scope.ServiceProvider;
-                //     var db = scopedServices.GetRequiredService<RegistrationApiContext>();
-                //     var logger = scopedServices
-                //         .GetRequiredService<ILogger<CustomWebApplicationFactory>>();
-
-                //     db.Database.EnsureCreated();
-                //}
             });
         }
     }
